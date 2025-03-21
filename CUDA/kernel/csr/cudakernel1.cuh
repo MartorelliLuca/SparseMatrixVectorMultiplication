ae19e7e023ed2fcba4404c4cb0bd5fbc1229e6fb
Luca Martorelli <lucam0109@gmail.com>
2025-03-21 12:11:56 +0100

    Refactored code

---
+++ CUDA/kernel/csr/cudakernel1.cuh
@@ -0,0 +1,12 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <cuda.h>
+#include <iostream>
+
+#include "cudacsr.h"
+
+double csr_matvec_cuda(CSRMatrix *csr, double *x, double *y)
+{
+}
+
+double csr_matvec_cuda(CSRMatrix *csr, double *x, double *y) {}
---
+++ CUDA/src/cudacsr.cu
@@ -0,0 +1,6 @@
+#include <cuda_runtime.h>
+#include <stdio.h>
+#include <cuda.h>
+#include <iostream>
+
+#include "cudacsr.h"
